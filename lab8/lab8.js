e25af81ea8a240055a4d302c3fc2acfd87b43372
Опарина Софья <pstereoo@gmail.com@>
2024-05-31 23:08:48 +0700

    Время

--- lab8/lab8.js
+++ lab8/lab8.js
@@ -47,12 +47,9 @@ function showTime() {
     let today = new Date();
     let currentTime = today.toLocaleTimeString('ru-RU');
     document.getElementById('time').innerHTML = currentTime;
-
     const seconds = today.getSeconds();
     const secondsDisplay = document.getElementById('seconds');
     const secondHand = document.getElementById('second-hand');
     secondsDisplay.innerHTML = seconds.toString().padStart(2, '0');
-    const secondsDegree = ((seconds / 60) * 360) + 90;
-    secondHand.style.transform = `rotate(${secondsDegree}deg)`;
 }
 setInterval(showTime, 1000);
