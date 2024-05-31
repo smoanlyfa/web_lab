43fb696462497df6d489b442cb34e4f2b9f596bf
Опарина Софья <pstereoo@gmail.com@>
2024-05-31 15:30:03 +0700

    Вывод текущей даты

--- lab8/lab8.js
+++ lab8/lab8.js
@@ -1,7 +1,7 @@
 function showDate() {
     let out = document.getElementById('current-date');
     let today = new Date();
-    out.innerHTML = today
+    const weekDays = ['Воскресенье', 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота'];
 
     out.innerHTML = `
         <div class="frame">
@@ -11,7 +11,10 @@ function showDate() {
             <div>Дата и время для локали Испании: ${today.toLocaleString('es-AR')}</div>
             <div>Дата и время для локали Китая: ${today.toLocaleString('zh-MO')}</div>
             <div>Дата и время для локали Словакии: ${today.toLocaleString('sk-SK')}</div>
-            
+            <div>Текущий год: ${today.getFullYear()}</div>
+            <div>Текущий месяц: ${today.getMonth()}</div>
+            <div>Текущая дата: ${today.getDate()}</div>
+            <div>День недели: ${weekDays[today.getDay()]}</div>
         </div>
     `;
 }
