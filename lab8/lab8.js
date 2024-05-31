86366257e2f75a7e567272ebe76f673cd2dd7eae
Опарина Софья <pstereoo@gmail.com@>
2024-05-31 23:00:35 +0700

    Время

--- lab8/lab8.js
+++ lab8/lab8.js
@@ -42,3 +42,17 @@ function clearInput() {
     document.getElementById('birthday').value = '';
     document.getElementById('count-days').textContent = '';
 }
+
+function showTime() {
+    let today = new Date();
+    let currentTime = today.toLocaleTimeString('ru-RU');
+    document.getElementById('time').innerHTML = currentTime;
+
+    const seconds = today.getSeconds();
+    const secondsDisplay = document.getElementById('seconds');
+    const secondHand = document.getElementById('second-hand');
+    secondsDisplay.innerHTML = seconds.toString().padStart(2, '0');
+    const secondsDegree = ((seconds / 60) * 360) + 90;
+    secondHand.style.transform = `rotate(${secondsDegree}deg)`;
+}
+setInterval(showTime, 1000);
